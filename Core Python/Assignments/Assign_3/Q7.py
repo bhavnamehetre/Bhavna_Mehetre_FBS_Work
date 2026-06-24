6c7e58247469e945a209ebb2422714bad8f9bda2
Bhavna Mehetre <bhavnamehetre3@gmail.com>
2026-06-24 20:08:44 +0530

    Add files via upload

---
+++ Core Python/Assignments/Assign_3/Q7.py
@@ -0,0 +1,8 @@
+user = "bhavna"
+passw = "bhavna@123"
+userid = input("enter the username::")
+p = input("enter the password::")
+if(user == userid and passw == p):
+    print("Login Successfull...!")
+else:
+    print("Invalid credemtial...!")
---
+++ Core Python/Assignments/Assign_3/vowel.py
@@ -0,0 +1,6 @@
+alpha = input("enter the input:")
+c = "aeiouAEIOU"
+if(alpha in c):
+    print("vowel.")
+else:
+    print("consonant.")
