2911c6ba8697971b457f59430056091902a456d2
Bhavna Mehetre <bhavnamehetre3@gmail.com>
2026-06-24 19:57:52 +0530

    Add files via upload

---
+++ Assign_3/Q3.py
@@ -0,0 +1,8 @@
+a = int(input("enter first angle::"))
+b = int(input("enter the second angle::"))
+c = int(input("enter the third angle::"))
+sum = a+b+c
+if(sum == 180):
+    print("Triangle is valid..!")
+else:
+    print("not valid...!")
