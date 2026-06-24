a885b29713dac881638dbd79cee4975e1847132c
Bhavna Mehetre <bhavnamehetre3@gmail.com>
2026-06-24 20:11:57 +0530

    Delete Assign_3 directory

--- Assign_3/positive_negative.py
+++
@@ -1,8 +0,0 @@
-a = int(input("enter the number::"))
-if(a >= 0):
-    print("number is positive.....!")
-elif(a == 0):
-    print("number is nuetral...!")
-else:
-    print("number is negative...!")
-
